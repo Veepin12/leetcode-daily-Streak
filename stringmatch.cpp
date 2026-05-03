143b37874c4edff63188e44470427a657a40c638
Veepin_Kumar <veepin8459@gmail.com>
2026-05-03 11:56:09 +0530

    rotate string'

---
+++ stringmatch.cpp
@@ -0,0 +1,40 @@
+//
+//  stringmatch.cpp
+//  DSA_with_Cpp
+//
+//  Created by Veepin_Chaudhary on 03/05/26.
+//
+/*
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+*/
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        return s.size() == goal.size() && (s + s).find(goal) != -1;
+    }
+};
+int main(){
+    string s = "abcde";
+    string goal = " cdeab";
+    Solution S;
+    cout<<S.rotateString(s,goal)<<endl;
+    return 0;
+}
