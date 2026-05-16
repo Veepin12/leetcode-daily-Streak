e9991dfacce50c9eda99d16cf8612e818c0a78de
Veepin_Kumar <veepin8459@gmail.com>
2026-05-16 15:07:34 +0530

    Minimum Search in O(logn)

---
+++ min_search_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+    
+public:
+
+    int min_search(vector<int> & nums){
+
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while( nums[start] >= nums[end]){
+            int mid  = start + (end + start) / 2;
+
+            if( nums[start] == nums[mid] && nums[mid] == nums[end]){
+               return *min_element(nums.begin() + start, nums.begin() + end);
+
+            }
+            if( nums[start] <= nums[mid]){
+                start = mid + 1;
+            }
+            else {
+                end = mid;
+            }
+        }
+        return nums[start];
+    }
+};
+int main(){
+    vector<int> nums = {17,4,5,0,5,4,9,0};
+    Solution S;
+
+    cout<<S.min_search(nums)<<endl;
+    return 0;
+
+}
