7b3ce3b09bcef9d047535ca48d6f87afb66aba84
Veepin_Kumar <veepin8459@gmail.com>
2026-09-03 11:32:51 +0530

    Construct_Uniform_Parity_Array_II

---
+++ Construct_Uniform_Parity_Array_II.cpp
@@ -0,0 +1,104 @@
+
+/*
+ You are given an array nums1 of n distinct integers.
+
+ You want to construct another array nums2 of length n such that the elements in nums2 are either all odd or all even.
+
+ For each index i, you must choose exactly one of the following (in any order):
+
+ nums2[i] = nums1[i]
+ nums2[i] = nums1[i] - nums1[j], for an index j != i, such that nums1[i] - nums1[j] >= 1
+ Return true if it is possible to construct such an array, otherwise return false.
+
+  
+
+ Example 1:
+
+ Input: nums1 = [1,4,7]
+
+ Output: true
+
+ Explanation:
+
+ Set nums2[0] = nums1[0] = 1.
+ Set nums2[1] = nums1[1] - nums1[0] = 4 - 1 = 3.
+ Set nums2[2] = nums1[2] = 7.
+ nums2 = [1, 3, 7], and all elements are odd. Thus, the answer is true.
+ Example 2:
+
+ Input: nums1 = [2,3]
+
+ Output: false
+
+ Explanation:
+
+ It is not possible to construct nums2 such that all elements have the same parity. Thus, the answer is false.
+
+ Example 3:
+
+ Input: nums1 = [4,6]
+
+ Output: true
+
+ Explanation:
+
+ Set nums2[0] = nums1[0] = 4.
+ Set nums2[1] = nums1[1] = 6.
+ nums2 = [4, 6], and all elements are even. Thus, the answer is true.
+  
+
+ Constraints:
+
+ 1 <= n == nums1.length <= 105
+ 1 <= nums1[i] <= 109
+ nums1 consists of distinct integers.
+ */
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    
+    bool cunstruct( vector<int> & nums){
+        
+        
+        int min_ele = INT_MAX;
+        
+        for(int  ele : nums){
+            min_ele = min( min_ele , ele);
+        }
+        if( min_ele % 2 == 1) return true;
+        
+        int even = 0;
+        
+        for( int ele : nums){
+            if( ele % 2 == 0){
+                even++;
+            }
+        }
+        
+        if( even == nums.size()) return true;
+        return false;
+        
+        
+        
+    }
+    
+};
+int main(){
+    int n;
+    cin>>n;
+    
+    vector<int> nums(n);
+    for( int i = 0; i < n; i++){
+        cin>>nums[i];
+    }
+    
+    Solution S;
+    
+    cout<<S.cunstruct(nums)<<endl;
+    
+    
+    
+    return 0;
+}
